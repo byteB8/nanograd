219190ea19aea4ed340ebe83a20adc9e8c81e07c
balbir <prasadbalbir1056@gmail.com>
2025-09-06 21:58:28 +0530

    add relu fn

--- nanograd/engine.py
+++ nanograd/engine.py
@@ -74,6 +74,14 @@ class Value:
         out._backward = _backward
         return out
 
+    def relu(self):
+        out = Value(max(0, self.data), (self,), 'relu')
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+        return out
+
     def exp(self):
         x = self.data
         out = Value(math.exp(x), (self,), 'exp')
